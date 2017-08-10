ca37c769632a4c41d440bc91806d84e482317146
MatejKozica <v-matej.kozica@dump.hr>
2017-08-10 19:19:56 +0200

    Dodana i napisana klasa Person

---
+++ Git-Tutorial-File/Git-Tutorial-File/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Tutorial_File
+{
+    class Person
+    {
+        public string Name;
+        public string Password;
+        public bool IsLoggedIn;
+    }
+}
