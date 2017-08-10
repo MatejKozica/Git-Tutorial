e522aab80e22e9fe7274b93aeafe9778af256daf
Ivan Vukorepa <ivukor00@fesb.hr>
2017-08-10 19:27:36 +0200

    dodan controller za login

---
+++ Git-Tutorial-File/Git-Tutorial-File/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Tutorial_File
+{
+    class Controller
+    {
+        public static void LogIn(Person person, string ime, string lozinka)
+        {
+            if (!person.IsLoggedIn)
+            {
+                if (person.Name == ime && person.Password == lozinka)
+                    person.IsLoggedIn = true;
+            }
+        }
+    }
+}
